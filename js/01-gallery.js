55a3624453bfde810a32efef694d32614a112911
Andrii Tsokur <tsokur@gmail.com>
2023-04-10 20:20:16 +0300

    Adds solved Task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+galleryList.addEventListener('click', showFullsizeImage);
+
+makeGalleryMarkup(galleryItems);
+
+function makeGalleryMarkup(items) {
+	const galleryItemsLayout = items
+		.map(({ preview, original, description }) => {
+			return `
+			<li class="gallery__item">
+				<a class="gallery__link" href="${original}">
+					<img
+						class="gallery__image"
+						src="${preview}"
+						data-source="${original}"
+						alt="${description}"
+					/>
+				</a>
+			</li>
+		`;
+		})
+		.join('');
+
+	galleryList.insertAdjacentHTML('afterbegin', galleryItemsLayout);
+}
+
+function showFullsizeImage(e) {
+	e.preventDefault();
+	if (e.target.nodeName !== 'IMG') {
+		return;
+	}
+
+	const instance = basicLightbox.create(`
+		<img src="${e.target.dataset.source}" alt="${e.target.alt}">
+	`);
+
+	instance.show();
+
+	escClosesFullsizeImage(instance);
+}
+
+function escClosesFullsizeImage(instance) {
+	window.addEventListener('keydown', function func(e) {
+		if (e.code === 'Escape') {
+			instance.close();
+			this.removeEventListener('keydown', func);
+		}
+	});
+}
